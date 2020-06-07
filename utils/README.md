96db8559d4fc934e55c0a16da329291d40b15cf8
Adam Lawson <adamjlawson22@gmail.com>
2020-06-07 17:50:27 +0100

    Continue cleaning

---
+++ utils/README.md
@@ -0,0 +1,1 @@
+A small library for loading and saving schematic files
---
+++ utils/utilsExamples.py
@@ -0,0 +1,7 @@
+import essentials
+
+array = essentials.makeArray('/home/ist/Desktop/AI/Minecraft/4,3,2.schematic')
+print("")
+essentials.makeBlocks(array, '/home/ist/Desktop/AI/Minecraft/example.schematic', 2,3,4)
+
+print(array)
